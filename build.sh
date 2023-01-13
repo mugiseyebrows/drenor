91ac78a146df53b0670b862c7f7f6c5a10495b03
Doronin Stanislav <mugisbrows@gmail.com>
2023-01-13 22:21:21 +0300

    initial

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+gcc -v
+perl -v
+git clone git://git.openssl.org/openssl.git
+cd openssl
+git checkout $1
+./config --prefix="`pwd`/../OpenSSL" --openssldir=ssl --shared
+sed -i 's,-mno-cygwin,,' Makefile
+sed -i 's,i486,i686,' Makefile
+sed -i 's,0x333,0x400,' Makefile
+make install
+cd ..
---
+++ main.bat
@@ -0,0 +1,4 @@
+@echo off
+rem This file is generated from main.pbat, all edits will be lost
+
+

f4ad28be942b9e4506b3f5d39c1cd49316148eeb
Doronin Stanislav <mugisbrows@gmail.com>
2023-02-17 14:41:44 +0300

    OpenSSL_1_0_1m

---
+++ main-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This file is generated from main.pbat, all edits will be lost
+gcc -v
+perl -v
+git clone git://git.openssl.org/openssl.git src
+cd src
+git checkout OpenSSL_1_0_1m
+./config --prefix="`pwd`/../OpenSSL" --openssldir=ssl --shared
+sed -i 's,-mno-cygwin,,' Makefile
+sed -i 's,i486,i686,' Makefile
+sed -i 's,0x333,0x400,' Makefile
+make
+cd ..
--- main.bat
+++ main.bat
@@ -1,4 +1,16 @@
 @echo off
 rem This file is generated from main.pbat, all edits will be lost
-
-
+if exist "C:\msys64\usr\bin\bash.exe" set MSYS2=C:\msys64\usr\bin\bash.exe
+if not defined MSYS2 (
+echo MSYS2 not found
+exit /b
+)
+set CHERE_INVOKING=yes
+if exist "C:\Program Files\7-Zip\7z.exe" set P7Z=C:\Program Files\7-Zip\7z.exe
+if not defined P7Z (
+echo P7Z not found
+exit /b
+)
+"%MSYS2%" %~dp0main-build.sh
+set MSYSTEM=MINGW32
+"%P7Z%" a -y "OpenSSL_1_0_1m.zip" OpenSSL
